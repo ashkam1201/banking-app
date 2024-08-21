64bed218079e0b08c907fdda5ffdc94db6707d31
adrian hagjija <ahagjija@openwt.com>
2024-08-21 23:43:15 +0200

    added main page + navbar left side & mobile version

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,8 @@
+import SideBar from "@/components/ui/SideBar";
+import Image from "next/image";
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
+import MobileNav from "@/components/ui/MobileNav";
 
 
 
@@ -8,10 +11,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    const loggedIn = { firstName: 'Adrian', lastName: 'Hagjija'}
+
   return (
-    <main>
-        sidebar
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <SideBar 
+            user={loggedIn} />
+
+        <div className="flex size-full flex-col">
+            <div className="root-layout">
+                <Image
+                    src="/icons/logo.svg"
+                    width={30}
+                    height={30}
+                    alt="logo" />
+                    <div>
+                        <MobileNav 
+                            user={loggedIn}/>
+                    </div>
+            </div>
+            {children}
+        </div>    
     </main>
   );
 }
